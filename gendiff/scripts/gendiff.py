d93c2e0a25df95383012e67e7fdccc7e6ab99c59
skarj <sergey.baranov@friendsurance.de>
2024-11-06 23:01:43 +0300

    Add json reading

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 from gendiff import cmd
-
+import json
 
 def main():
-    cmd.parse_args()
+    args = cmd.get_args()
+    file1 = args.first_file
+    file2 = args.second_file
 
+    json1 = json.load(open(file1))
+    json2 = json.load(open(file2))
+
+    print(json1, json2)
 
 if __name__ == '__main__':
     main()
