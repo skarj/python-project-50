a03a9b83d6b80c4d3efe31bd3ef1894ba161d916
skarj <sergey.baranov@friendsurance.de>
2024-11-06 22:46:34 +0300

    Add format argument

--- gendiff/cmd.py
+++ gendiff/cmd.py
@@ -6,4 +6,5 @@ def parse_args():
                     description='Compares two configuration files and shows a difference')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.parse_args()
