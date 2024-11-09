31e51efdf21777b781229529ebab267c2e848623
skarj <sergey.baranov@friendsurance.de>
2024-11-09 22:38:24 +0300

    Refactor

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,30 +5,30 @@ from gendiff.generate_diff import generate_diff
 @pytest.fixture(name='input_files')
 def _input_files():
     return {
-        'json1': 'tests/fixtures/file1.json',
-        'json2': 'tests/fixtures/file2.json',
-        'yaml1': 'tests/fixtures/file1.yaml',
-        'yaml2': 'tests/fixtures/file2.yaml',
+        'json1': 'tests/fixtures/simple1.json',
+        'json2': 'tests/fixtures/simple2.json',
+        'yaml1': 'tests/fixtures/simple1.yaml',
+        'yaml2': 'tests/fixtures/simple2.yaml',
     }
 
 
-@pytest.fixture(name='result1')
-def _result1():
-    with open('tests/fixtures/result1') as result:
+@pytest.fixture(name='simple_result')
+def _simple_result():
+    with open('tests/fixtures/simple_result') as result:
         return result.read().strip()
 
 
-def test_compare_correct_json_files(input_files, result1):
-    assert generate_diff(input_files['json1'], input_files['json2']) == result1
+def test_compare_correct_json_files(input_files, simple_result):
+    assert generate_diff(input_files['json1'], input_files['json2']) == simple_result
 
 
-def test_compare_correct_yaml_files(input_files, result1):
-    assert generate_diff(input_files['yaml1'], input_files['yaml2']) == result1
+def test_compare_correct_yaml_files(input_files, simple_result):
+    assert generate_diff(input_files['yaml1'], input_files['yaml2']) == simple_result
 
 
-def test_compare_correct_yam_json_files(input_files, result1):
-    assert generate_diff(input_files['yaml1'], input_files['json2']) == result1
+def test_compare_correct_yam_json_files(input_files, simple_result):
+    assert generate_diff(input_files['yaml1'], input_files['json2']) == simple_result
 
 
-def test_unsupported_file_format(input_files, result1):
+def test_unsupported_file_format(input_files):
     assert generate_diff(input_files['yaml1'], 'tests/fixtures/file2.xml') == 'Error! Unsupported format type: .xml'
