e7e935041d54885b71bdf421c923ba1870540a65
skarj <sergey.baranov@friendsurance.de>
2024-11-18 13:42:11 +0300

    Refactor to an intenal view of diff

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+from collections import defaultdict
 
 
 def get_parser(format):
@@ -14,33 +15,54 @@ def get_parser(format):
     return loader
 
 
-def render_line(key, value, symbol=' ', indent=2):
-    indent = ' ' * indent
-
-    if isinstance(value, int):
-        value = str(value).lower()
-
-    return f'{indent}{symbol} {key}: {value}'
-
-
-def compare_objects(obj1, obj2):
+def create_diff(obj1, obj2):
     removed = obj1.keys() - obj2.keys()
     added = obj2.keys() - obj1.keys()
     same = obj2.keys() & obj1.keys()
     all = removed | added | same
 
-    result = ['{']
-    for k in sorted(all):
+    diff = defaultdict(dict)
+    for k in all:
         if k in removed:
-            result.append(render_line(k, obj1[k], '-'))
-        if k in added:
-            result.append(render_line(k, obj2[k], '+'))
-        if k in same:
-            if obj1[k] != obj2[k]:
-                result.append(render_line(k, obj1[k], '-'))
-                result.append(render_line(k, obj2[k], '+'))
-            else:
-                result.append(render_line(k, obj1[k]))
+            diff["removed"].update({k: obj1[k]})
+        elif k in added:
+            diff["added"].update({k: obj2[k]})
+        elif k in same and obj1[k] != obj2[k]:
+            diff["removed"].update({k: obj1[k]})
+            diff["added"].update({k: obj2[k]})
+        else:
+            diff["unchanged"].update({k: obj1[k]})
+
+    return diff
+
+
+def render_stylish_line(key, value, symbol=' ', indent=2):
+    indent = ' ' * indent
+
+    if isinstance(value, bool):
+        value = str(value).lower()
+
+    return f'{indent}{symbol} {key}: {value}'
+
+
+def format_stylish(diff, indent=1):
+    all_keys = set(key for sub_dict in diff.values() for key in sub_dict.keys())
+
+    result = ['{']
+    for k in sorted(all_keys):
+        if k in diff['removed']:
+            result.append(render_stylish_line(k, diff['removed'][k], '-'))
+        if k in diff['added']:
+            result.append(render_stylish_line(k, diff['added'][k], '+'))
+        if k in diff['unchanged']:
+            result.append(render_stylish_line(k, diff['unchanged'][k]))
     result.append('}')
+
+    return result
+
+
+def compare_objects(obj1, obj2):
+    diff = create_diff(obj1, obj2)
+    result = format_stylish(diff, 2)
 
     return '\n'.join(result)
