c99a0530140b287301c60ac01a0d2368fd4b3abf
skarj <sergey.baranov@friendsurance.de>
2024-11-22 21:22:01 +0300

    Simplify stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,23 +1,21 @@
 from gendiff.config import INDENT
 
-def render_stylish(json, depth=1, diff_symbol=' ', indent_size=4):
-    def parse(data, result, depth, diff_symbol):
-        indent = ' ' * (indent_size * depth - 2)
-        for k, v in data.items():
-            if isinstance(v, dict):
-                result.append(f'{indent}{diff_symbol} {k}: {{')
-                parse(v, result, depth + 1, ' ')
-                result.append(f'{indent}  }}')
-            else:
-                if isinstance(v, bool):
-                    v = str(v).lower()
-                elif v is None:
-                    v = 'null'
-                result.append(f'{indent}{diff_symbol} {k}: {v}')
-        return result
 
-    result = []
-    result = parse(json, result, depth, diff_symbol)
+def render_stylish(data, result=None, depth=1, diff_symbol=' '):
+    result = result or []
+
+    indent = ' ' * (INDENT * depth - 2)
+    for k, v in data.items():
+        if isinstance(v, dict):
+            result.append(f'{indent}{diff_symbol} {k}: {{')
+            render_stylish(v, result, depth + 1, ' ')
+            result.append(f'{indent}  }}')
+        else:
+            if isinstance(v, bool):
+                v = str(v).lower()
+            elif v is None:
+                v = 'null'
+            result.append(f'{indent}{diff_symbol} {k}: {v}')
 
     return result
 
@@ -33,12 +31,12 @@ def format_stylish(diff):
                 result.append(f'{indent}  }}')
             elif 'state' in v:
                 diff_symbol = '-' if v['state'] == 'removed' or v['state'] == 'updated' else '+'
-                result.extend(render_stylish({k: v['value']}, depth, diff_symbol))
+                result.extend(render_stylish({k: v['value']}, depth=depth, diff_symbol=diff_symbol))
 
                 if v['state'] == 'updated':
-                    result.extend(render_stylish({k: v['new_value']}, depth, '+'))
+                    result.extend(render_stylish({k: v['new_value']}, depth=depth, diff_symbol='+'))
             else:
-                result.extend(render_stylish({k: v}, depth, ' '))
+                result.extend(render_stylish({k: v}, depth=depth, diff_symbol=' '))
 
         return result
 
