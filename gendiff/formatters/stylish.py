659ccf8c47305a1cfc018cf81409721b30a63c8e
skarj <sergey.baranov@friendsurance.de>
2024-11-27 20:43:07 +0300

    revert

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,49 +1,45 @@
-from gendiff.states import UPDATED, REMOVED, ADDED
+from gendiff.states import UPDATED, REMOVED
 
 INDENT = 4
 
 
-def stringify(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    elif value is None:
-        return 'null'
-    else:
-        return value
+def stringify(data, result=None, depth=1, diff_symbol=' '):
+    if result is None:
+        result = []
+    indent = ' ' * (INDENT * depth - 2)
+
+    for key, node in data.items():
+        if isinstance(node, dict):
+            result.append(f'{indent}{diff_symbol} {key}: {{')
+            stringify(node, result, depth + 1, ' ')
+            result.append(f'{indent}  }}')
+        elif isinstance(node, bool):
+            result.append(f'{indent}{diff_symbol} {key}: {str(node).lower()}')
+        elif node is None:
+            result.append(f'{indent}{diff_symbol} {key}: null')
+        else:
+            result.append(f'{indent}{diff_symbol} {key}: {node}')
+
+    return result
 
 
 def format_stylish(diff):
-    def format(data, result=None, depth=1, diff_symbol=' '):
-        if result is None:
-            result = []
-
+    def format(data, result, depth=1):
         indent = ' ' * (INDENT * depth - 2)
-        for key, node in sorted(data.items()):
-            if isinstance(node, dict):
-                if 'children' in node:
-                    result.append(f'{indent}  {key}: {{')
-                    format(node['children'], result, depth + 1)
-                    result.append(f'{indent}  }}')
-                elif 'state' in node:
-                    state = node.get('state')
-                    value = node.get('value')
-                    new_value = node.get('new_value')
 
-                    if state == REMOVED:
-                        format({key: value}, result, depth, '-')
-                    elif state == UPDATED:
-                        format({key: value}, result, depth, '-')
-                        format({key: new_value}, result, depth, '+')
-                    elif state == ADDED:
-                        format({key: value}, result, depth, '+')
-                    else:
-                        format({key: value}, result, depth, ' ')
-                else:
-                    result.append(f'{indent}{diff_symbol} {key}: {{')
-                    format(node, result, depth + 1, diff_symbol=' ')
-                    result.append(f'{indent}  }}')
+        for key, node in sorted(data.items()):
+            if 'children' in node:
+                result.append(f'{indent}  {key}: {{')
+                format(node['children'], result, depth + 1)
+                result.append(f'{indent}  }}')
+            elif 'state' in node:
+                diff_symbol = '-' if node['state'] in {REMOVED, UPDATED} else '+'
+                result.extend(stringify({key: node['value']}, depth=depth, diff_symbol=diff_symbol))
+
+                if node['state'] == UPDATED:
+                    result.extend(stringify({key: node['new_value']}, depth=depth, diff_symbol='+'))
             else:
-                result.append(f'{indent}{diff_symbol} {key}: {stringify(node)}')
+                result.extend(stringify({key: node}, depth=depth))
 
         return result
 
