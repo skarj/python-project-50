15142693b509c5f7733c4d77ad666ed148e9c277
skarj <sergey.baranov@friendsurance.de>
2024-11-30 22:11:46 +0300

    refactor

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -23,6 +23,15 @@ def stringify(data, result=None, depth=1, diff_symbol=' '):
     return result
 
 
+# def stringify(value):
+#     if isinstance(value, bool):
+#         return str(value).lower()
+#     elif value is None:
+#         return 'null'
+#     else:
+#         return value
+
+
 def format_stylish(diff):
     def inner(data, depth=1):
         indent = ' ' * (INDENT * depth - 2)
@@ -31,14 +40,19 @@ def format_stylish(diff):
             if 'state' in node:
                 value = node['value']
                 if node['state'] == REMOVED:
-                    result.extend(stringify({key: value}, depth=depth, diff_symbol='-'))
+                    diff_symbol = '-'
                 elif node['state'] == UPDATED:
-                    result.extend(stringify({key: value[0]}, depth=depth, diff_symbol='-'))
-                    result.extend(stringify({key: value[1]}, depth=depth, diff_symbol='+'))
+                    diff_symbol = '-'
+                    new_value = value[1]
+                    value = value[0]
                 elif node['state'] == ADDED:
-                    result.extend(stringify({key: value}, depth=depth, diff_symbol='+'))
+                    diff_symbol = '+'
                 elif node['state'] == UNCHANGED:
-                    result.extend(stringify({key: value}, depth=depth))
+                    diff_symbol = ' '
+
+                result.extend(stringify({key: value}, depth=depth, diff_symbol=diff_symbol))
+                if node['state'] == UPDATED:
+                    result.extend(stringify({key: new_value}, depth=depth, diff_symbol='+'))
             else:
                 result.append(f'{indent}  {key}: {{')
                 inner(node, depth=depth + 1)
