65c7e2e1657430e8955bc1f7bc747d90b659c980
skarj <sergey.baranov@friendsurance.de>
2024-12-01 12:54:25 +0300

    Refactor stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -17,35 +17,32 @@ def format_stylish(diff):
         result = []
 
         for key, node in sorted(data.items()):
-            if isinstance(node, dict):
-                if 'state' in node:
-                    value = node['value']
-                    if node['state'] == REMOVED:
-                        diff_symbol = '-'
-                    elif node['state'] == UPDATED:
-                        diff_symbol = '-'
-                        new_value = value[1]
-                        value = value[0]
-                    elif node['state'] == ADDED:
-                        diff_symbol = '+'
-                    else:
-                        diff_symbol = ' '
+            if isinstance(node, dict) and 'state' in node:
+                value = node['value']
+                diff_symbol = {
+                    REMOVED: '-', UPDATED: '-', ADDED: '+'
+                }.get(node['state'], ' ')
 
-                    if isinstance(value, dict):
-                        result.append(f'{indent}{diff_symbol} {key}: {{')
-                        result.extend(inner(value, depth + 1))
-                        result.append(f'{indent}  }}')
-                    else:
-                        result.append(f'{indent}{diff_symbol} {key}: {stringify(value)}')
+                if node['state'] == UPDATED:
+                    new_value = value[1]
+                    value = value[0]
 
-                    if node['state'] == UPDATED:
-                        result.append(f'{indent}+ {key}: {stringify(new_value)}')
+                if isinstance(value, dict):
+                    result.append(f'{indent}{diff_symbol} {key}: {{')
+                    result.extend(inner(value, depth + 1))
+                    result.append(f'{indent}  }}')
                 else:
+                    result.append(f'{indent}{diff_symbol} {key}: {stringify(value)}')
+
+                if node['state'] == UPDATED:
+                    result.append(f'{indent}+ {key}: {stringify(new_value)}')
+            else:
+                if isinstance(node, dict):
                     result.append(f'{indent}  {key}: {{')
                     result.extend(inner(node, depth + 1))
                     result.append(f'{indent}  }}')
-            else:
-                result.append(f'{indent}  {key}: {stringify(node)}')
+                else:
+                    result.append(f'{indent}  {key}: {stringify(node)}')
 
         return result
 
