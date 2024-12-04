89f2e47a53c51802f6d3e0bb3211b7f235e9f8bf
Sergey Baranov <37897781+skarj@users.noreply.github.com>
2024-12-04 23:15:59 +0300

    Try namedtuples for diff view (#3)

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,5 +1,22 @@
 import json
 
 
+def format_nodes(nodes):
+    result = {}
+
+    for node in nodes:
+        value = node.value
+        key = node.key
+
+        if isinstance(value, list):
+            value = format_nodes(value)
+
+        result[key] = {'value': value, 'type': node.type}
+
+    return result
+
+
 def format_json(data):
-    return json.dumps(data)
+    result = format_nodes(data)
+
+    return json.dumps(result)
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,3 +1,4 @@
+from gendiff.models import Node
 from gendiff.types import ADDED, NESTED, REMOVED, UNCHANGED, UPDATED
 
 INDENT = 4
@@ -21,14 +22,12 @@ def get_indent(depth=1):
 
 
 def format_updated_node(node, depth=1):
-    node_key, node_props = node
-    value_old, value_new = node_props['value']
-
+    value_old, value_new = node.value
     removed = format_node(
-        (node_key, {"value": value_old, "type": REMOVED}), depth
+        Node(node.key, value_old, REMOVED), depth
     )
     added = format_node(
-        (node_key, {"value": value_new, "type": ADDED}), depth
+        Node(node.key, value_new, ADDED), depth
     )
 
     return f'{removed}\n{added}'
@@ -36,42 +35,31 @@ def format_updated_node(node, depth=1):
 
 def format_node(node, depth=1):
     indent = get_indent(depth)
+    sign = get_sign(node.type)
+    node_value = node.value
 
-    node_key, node_props = node
-    node_value = node_props['value']
-    sign = get_sign(node_props['type'])
-
-    if isinstance(node_value, dict):
-        nodes = {}
-        for key, value in node_value.items():
-            nodes[key] = {'value': value, 'type': UNCHANGED}
+    if isinstance(node.value, dict):
+        nodes = []
+        for key, value in node.value.items():
+            nodes.append(Node(key, value, UNCHANGED))
 
         node_value = format_stylish(nodes, depth + 1)
 
-    return f'{indent}{sign} {node_key}: {stringify(node_value)}'
+    return f'{indent}{sign} {node.key}: {stringify(node_value)}'
 
 
 def format_nested_node(node, depth=1):
-    node_key, node_props = node
-    node_value = node_props['value']
-    node_type = node_props['type']
-
     return format_node(
-        (node_key, {
-            "value": format_stylish(node_value, depth + 1),
-            "type": node_type
-        }), depth
+        Node(node.key, format_stylish(node.value, depth + 1), node.type), depth
     )
 
 
 def format_stylish(diff, depth=1):
     result = []
-    for node in sorted(diff.items()):
-        _, value = node
-        node_type = value['type']
-        if node_type == NESTED:
+    for node in sorted(diff):
+        if node.type == NESTED:
             result.append(format_nested_node(node, depth))
-        elif node_type == UPDATED:
+        elif node.type == UPDATED:
             result.append(format_updated_node(node, depth))
         else:
             result.append(format_node(node, depth))
