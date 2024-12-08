ba41ef81251438112f595bc8efcf3c0bcd4b4df2
skarj <sergey.baranov@friendsurance.de>
2024-12-08 17:47:54 +0300

    Move extention check to the parser

--- gendiff/file.py
+++ gendiff/file.py
@@ -4,13 +4,11 @@ import os
 def load_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
+
     return content
 
 
 def get_extention(file_path):
     _, extention = os.path.splitext(file_path)
-
-    if extention not in {'.json', '.yaml', '.yml'}:
-        raise ValueError(f'Unsupported format type: {extention}')
 
     return extention
--- gendiff/parse.py
+++ gendiff/parse.py
@@ -4,7 +4,9 @@ import yaml
 
 
 def parse_file(text, extention):
-    if extention in {'.yaml', '.yml'}:
+    if extention not in {'.json', '.yaml', '.yml'}:
+        raise ValueError(f'Unsupported format type: {extention}')
+    elif extention in {'.yaml', '.yml'}:
         return yaml.safe_load(text)
     else:
         return json.loads(text)
